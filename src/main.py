404557e68dbff76f5fbad35adde991084ea85ec3
Bobby Panczer <bobbypanczer@gmail.com>
2018-10-23 23:15:41 -0400

    filtering functions

--- src/main.py
+++ src/main.py
@@ -10,6 +10,8 @@ post_author = bs4_obj.select('.name')
 post_details = bs4_obj.select('.postdetails')
 post_body = bs4_obj.select('.postbody')
 
+scrubbed_authors = []
+scrubbed_details = []
 with open('thread.csv', 'w', newline='') as csvfile:
     csv_writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
     post = ''
@@ -18,5 +20,26 @@ with open('thread.csv', 'w', newline='') as csvfile:
     print(len(post_details))
     print(len(post_body))
     print('----------')
+
+    for author in post_author:
+      scrubbed_authors.append(author.get_text())
+    print(scrubbed_authors)
+
+    cut_words = ['Posted: ','am','pm']
+    for detail in post_details:
+      if 'Posted:' in detail.get_text():
+        detail = detail.get_text()   
+        cut_locations = []
+        for cut_word in cut_words:
+          if cut_word in detail:
+            start = detail.find(cut_word)
+            cut_location = start + len(cut_word)
+            cut_locations.append(cut_location)
+        detail = detail[cut_locations[0]:cut_locations[1]]
+        scrubbed_details.append(detail)
+    print(scrubbed_details)
+    # for body in post_body:
+    #   print(body.get_text())
+    #   print('@@@@@@@@@@@@@@@@@@@@')
 
     csv_writer.writerow(['Spam'])
