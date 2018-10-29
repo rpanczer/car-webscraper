13c994a833e0c496c90547e6e6ac8c6adf3fca6a
Bobby Panczer <bobbypanczer@gmail.com>
2018-10-28 22:26:34 -0400

    scraping over multiple pages

--- src/extracts.py
+++ src/extracts.py
@@ -10,27 +10,27 @@ def textExtract(tag_list):
 def numberExtract(tag_list,tag_name):
     extracted_text = []
     for tag in tag_list:
-      extracted_text = tag.get(tag_name)
-    for element in extracted_text:
-      if not element.isdigit():
-        del element
+      tag = tag.get(tag_name)
+      if tag is not None and tag.isdigit():
+        extracted_text.append(tag)
     return extracted_text
 
 def replaceNewlineExtract(tag_list):
-  extracted_text = textExtract(tag_list)
-  for element in extracted_text:
+  extracted_text = []
+  tag_list_text = textExtract(tag_list)
+  for element in tag_list_text:
     if len(element) > 0:
       element = element.replace('\n', '\\n')
-    else: 
-      del element
-    return extracted_text
+      extracted_text.append(element)
+  return extracted_text
 
 # uses a list of 2 or 3 cut_words
 def spliceExtract(tag_list,cut_words):
-  if len(cut_words) != 2 or len(cut_words) !=3:
+  extracted_text = []
+  if len(cut_words) != 2 and len(cut_words) !=3:
     return False
-  extracted_text = textExtract(tag_list)
-  for text in extracted_text:
+  tag_list_text = textExtract(tag_list)
+  for text in tag_list_text:
     if cut_words[0] in text:
       cut_locations = []
       for cut_word in cut_words:
@@ -39,6 +39,5 @@ def spliceExtract(tag_list,cut_words):
           cut_location = start + len(cut_word)
           cut_locations.append(cut_location)
       text = text[cut_locations[0]:cut_locations[1]]
-    else:
-      del text
+      extracted_text.append(text)
   return extracted_text
--- src/main.py
+++ src/main.py
@@ -1,40 +1,60 @@
 import requests, bs4, csv
+import json
 from extracts import textExtract, numberExtract, replaceNewlineExtract, spliceExtract
 
-site = 'http://www.oldclassiccar.co.uk/forum/phpbb/phpBB2/viewtopic.php?t=12591&postdays=0&postorder=asc&start=0'
-site_src = requests.get(site)
-site_src.raise_for_status()
+def main():
+  post = 0
+  posts = []
+  continue_scraping = True
+  while continue_scraping:
+    url = 'http://www.oldclassiccar.co.uk/forum/phpbb/phpBB2/viewtopic.php?t=12591&postdays=0&postorder=asc&start=' + str(post)
+    bs4_obj = make_soup(url)
+    page_post_list = create_post_objects(bs4_obj)
+    if len(page_post_list) > 0:
+      for post in page_post_list:
+        posts.append(post)
+        post = len(posts) + 1  
+    else:
+      continue_scraping = False
+      write_posts_to_csv(posts)
 
-bs4_obj = bs4.BeautifulSoup(site_src.text)
+def make_soup(url):
+  print("cooking some soup...")
+  print(url)
+  site_src = requests.get(url)
+  site_src.raise_for_status()
+  bs4_obj = bs4.BeautifulSoup(site_src.text, 'html.parser')
+  return bs4_obj
+  
+def create_post_objects(bs4_obj):
+  errors = {}
+  page_post_list = []
+  page_rows = bs4_obj.find_all('tr')
+  for row in page_rows:
+    post_id = row.find_all('a')
+    post_author = row.select('.name')
+    post_details = row.select('.postdetails')
+    post_body = row.select('.postbody')
+    if post_id is not None and post_author is not None and post_details is not None and post_body is not None:
+      post_details_cut_words = ['Posted: ','am','pm']
+      post = {}
+      post['id'] = numberExtract(post_id,'name')
+      post['author'] = textExtract(post_author)
+      post['created_at'] = spliceExtract(post_details,post_details_cut_words)
+      post['body'] = replaceNewlineExtract(post_body)
+      print(json.dumps(post, indent=2))
+      page_post_list.append(post)
+  return page_post_list, errors
 
-post_id = bs4_obj.find_all('a')
-post_author = bs4_obj.select('.name')
-post_details = bs4_obj.select('.postdetails')
-post_body = bs4_obj.select('.postbody')
-
-scrubbed_body = []
-scrubbed_ids = []
-scrubbed_authors = []
-scrubbed_details = []
-with open('thread.csv', 'w', newline='') as csvfile:
-    csv_writer = csv.writer(csvfile, dialect='excel', quotechar=' ', doublequote=False, delimiter=' ', escapechar='\\')
-
-    post_details_cut_words = ['Posted: ','am','pm']
-    scrubbed_ids = numberExtract(post_id,'name')
-    scrubbed_authors = textExtract(post_author)
-    scrubbed_body = replaceNewlineExtract(post_body)
-    scrubbed_details = spliceExtract(post_details,post_details_cut_words)
-    # scrubbed_details = regexExtract(post_details)
-
-        
-    if (len(scrubbed_body) == len(scrubbed_ids) == len(scrubbed_authors) == len(scrubbed_details)) :
-      post_length = len(scrubbed_ids)
-      i = 0
-      while i < post_length:
-        row = scrubbed_ids[i] + ",'" + scrubbed_authors[i] + "','" + scrubbed_details[i] + "','" + scrubbed_body[i] + "'"
-        print(row)
+def write_posts_to_csv(posts):
+  with open('thread.csv', 'w', newline='') as csvfile:
+      csv_writer = csv.writer(csvfile, dialect='excel', quotechar=' ', doublequote=False, delimiter=' ', escapechar='\\')
+      for post in posts:
+        row = post['id'] + ",'" + post['author'] + "','" + post['created_at'] + "','" + post['body'] + "'"
         csv_writer.writerow(row)
-        i += 1
+
+if __name__ == '__main__':
+    main()
     
 
     
